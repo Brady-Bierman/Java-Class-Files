b48d42759fb8c6633acfe9b1d8bcb88a021ff6d6
Brady-Bierman <166095065+Brady-Bierman@users.noreply.github.com>
2024-08-21 15:56:08 -0400

    Add files via upload

---
+++ week 1/Hello2.java
@@ -0,0 +1,15 @@
+//Filename Hello2.java
+//Written by Brady Bierman
+//Written on 8/21/24
+
+
+public class Hello2
+/*this class demonstrates the use of println() method to print the message Hello, World! */
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
+
+
---
+++ week 1/HelloDialog.java
@@ -0,0 +1,11 @@
+//Filename Hello2.java
+//Written by Brady Bierman
+//Written on 8/21/24
+
+import javax.swing.JOptionPane;
+
+public class HelloDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello, World!");
+    }
+}
